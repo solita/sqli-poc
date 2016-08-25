f11d9e5b1a07751635691c68f6593f786e52d1d6
Antti Virtanen <antti.virtanen@solita.fi>
2016-08-25 10:25:16 +0300

    order by from user..

--- src/main/java/poc/APIController.java
+++ src/main/java/poc/APIController.java
@@ -1,19 +1,38 @@
 package poc;
 
 
-import com.querydsl.jpa.impl.JPAQuery;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.persistence.EntityManager;
+import com.querydsl.core.types.OrderSpecifier;
+import com.querydsl.core.types.dsl.PathBuilder;
+import com.querydsl.jpa.impl.JPAQuery;
 
 @RestController
 public class APIController {
 
     @Autowired
     private EntityManager entityManager;
+
+    @RequestMapping("/personlist")
+    public List<Person> personList(@RequestParam(value="order", defaultValue="firstName") String order) {
+        QPerson person = QPerson.person;
+        JPAQuery<?> query = new JPAQuery<Void>(entityManager);
+        PathBuilder orderByExpression = new PathBuilder(Object.class, "person");
+        OrderSpecifier o = new OrderSpecifier(com.querydsl.core.types.Order.DESC, 
+            orderByExpression.get(order));
+        List<Person> result = query.select(person)
+                .from(person)
+                .orderBy(o)
+                .fetch();
+        return result;
+    }
 
     @RequestMapping("/person")
     public Person person(@RequestParam(value="name", defaultValue="Hupu") String name) {
