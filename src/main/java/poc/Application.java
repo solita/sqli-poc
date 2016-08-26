157e0eaaee0c8e29d6c8a823a84e1ec028663bff
Antti Virtanen <antti.virtanen@solita.fi>
2016-08-26 11:08:36 +0300

    Match PoC data with README.md screenshot. Removed unnecessary code.

--- src/main/java/poc/Application.java
+++ src/main/java/poc/Application.java
@@ -1,10 +1,7 @@
 package poc;
 
-import javax.persistence.EntityManager;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -12,9 +9,6 @@ import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
-
-	@Autowired
-	private EntityManager entityManager;
 
 	private static final Logger log = LoggerFactory.getLogger(Application.class);
 
@@ -25,8 +19,8 @@ public class Application {
 	@Bean
 	public CommandLineRunner demo(PersonRepository personRepository) {
 		return (args) -> {
-			personRepository.save(new Person("Hupu", "Ankka", "SAD"));
-			personRepository.save(new Person("Tupu", "Ankka", "EVIL"));
+			personRepository.save(new Person("Huppu", "Ankka", "SAD"));
+			personRepository.save(new Person("Tuppu", "Ankka", "EVIL"));
 			personRepository.save(new Person("Lupus", "Ankka", "THINGS")); 
 		};
 	}
